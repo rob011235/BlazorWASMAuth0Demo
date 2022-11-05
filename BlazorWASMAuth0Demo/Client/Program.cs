43eab22e35a0f458ccc82d1acecadad294a6dbf8
rob011235 <rgarner011235@gmail.com>
2022-11-05 15:55:06 -0600

    Add project files.

---
+++ BlazorWASMAuth0Demo/Client/Program.cs
@@ -0,0 +1,19 @@
+using BlazorWASMAuth0Demo.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Begin for Auth0
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+//End for Auth0
+
+await builder.Build().RunAsync();
